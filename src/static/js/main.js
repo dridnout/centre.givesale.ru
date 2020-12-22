4a3e046a7e5e776921a821fdf94267c40779bf81
alexander zalomanin <dridnout@yandex.ru>
2020-12-22 21:06:10 +0400

    change grid

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -4,14 +4,14 @@ $(document).ready(function(){
     
     svg4everybody({});
 
-    enquire.register("screen and (max-width: 1200px)", {
+    enquire.register("screen and (max-width: 1500px)", {
 
 		// OPTIONAL
 		// If supplied, triggered when a media query matches.
 		match : function() {
             // $('.ccard .ccard-contacts').appendTo('.ccard .ccard-right');
             $('.form-block').insertAfter('.centers-container');
-            $('.footer').insertAfter('.form-block');
+            // $('.footer').insertAfter('.form-block');
 		},
 
 		// OPTIONAL
@@ -19,7 +19,7 @@ $(document).ready(function(){
 		// *from a matched state to an unmatched state*.
 		unmatch : function() {
             $('.form-block').insertAfter('.centers-cards-wrap');
-            $('.footer').insertAfter('.form-block');
+            // $('.footer').insertAfter('.form-block');
 
         }
 	});
