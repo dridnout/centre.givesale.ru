82a9a7eb27e16b3875babad9c9c57d8ea9acc704
alexander zalomanin <dridnout@yandex.ru>
2020-12-30 02:14:03 +0400

    fix icons

--- gulp/tasks/css.js
+++ gulp/tasks/css.js
@@ -4,12 +4,7 @@ module.exports = function(){
 			'src/static/libs/normalize/normalize.css',
 			'src/static/libs/pgs-panel/css/pgs-main.css',
 			'src/static/libs/pgs-panel/css/jquery.mCustomScrollbar.css',
-			'src/static/libs/bootstrap/css/bootstrap.min.css',
-			// 'src/static/libs/bootstrap/css/bootstrap-grid.min.css'
-			// 'src/static/libs/bootstrap/bootstrap-grid.min.css'
-			// 'src/static/libs/swiper-master/package/css/swiper.min.css',
-			// 'src/static/libs/fancybox-master/dist/jquery.fancybox.min.css',
-			// 'src/static/libs/jquery.scrollbar-gh-pages/jquery.scrollbar.css'
+			'src/static/libs/bootstrap/css/bootstrap.min.css'
 		])
 		
 		.pipe($.concat('libs.css'))
--- gulp/tasks/script.js
+++ gulp/tasks/script.js
@@ -5,17 +5,7 @@ module.exports = function(){
 			'src/static/libs/pgs-panel/js/pgs-main.js',
 			'src/static/libs/bootstrap/js/bootstrap.min.js',
 			'src/static/libs/pgs-panel/js/jquery.mCustomScrollbar.concat.min.js',
-			// 'src/static/libs/swiper-master/package/js/swiper.min.js',
-			// 'src/static/libs/enquire/enquire.min.js',
-			// 'src/static/libs/fancybox-master/dist/jquery.fancybox.min.js',
-			// 'src/static/libs/inputmask/jquery.inputmask.js',
-			// 'src/static/libs/rating/rater.js',
-			// 'src/static/libs/jquery-doubletaptogo-master/dist/jquery.dcd.doubletaptogo.min.js',
-			'src/static/libs/svg4everybody/svg4everybody.min.js',
-			// 'src/static/libs/sticky-js-master/dist/sticky.min.js',
-			// 'src/static/libs/Columnizer-jQuery-Plugin-master/src/jquery.columnizer.min.js',
-			// 'src/static/libs/sticky/uikit.min.js'
-			// 'src/static/libs/jquery.scrollbar-gh-pages/jquery.scrollbar.js'
+			'src/static/libs/svg4everybody/svg4everybody.min.js'
 		])
 		.pipe($.concat('libs.min.js'))
 		.pipe($.uglify()) 
